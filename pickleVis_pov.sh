78403007d0997281f96cb8c12db4e95e620a9974
Dona Greta Isai <greta@bubba.intra3.kumc>
2016-08-25 18:04:27 -0500

    restore previous state

--- pickleVis_pov.sh
+++
@@ -1,6 +0,0 @@
-T=$(tempfile); 
-R=$(cat $2)
-view=${3:-v}	#default is v
-$(dirname $0)/pickleVis_pov.py -i $1 -l 2 -r $R -w $view> $T; 
-povray -d -GD -GR -GS +I$T +H480 +W480 -O-  2> /dev/null
-rm $T
